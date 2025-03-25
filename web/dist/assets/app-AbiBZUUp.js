ab6277e1013b0b7741ef0e63578223d00714416a
Andre Nguyen <a@ackstudio.com>
2025-03-25 18:25:56 +0800

    Build

---
+++ web/dist/assets/app-AbiBZUUp.js
@@ -0,0 +1,2 @@
+function o(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}console.log("hello");export{o as __vite_legacy_guard};
+//# sourceMappingURL=app-AbiBZUUp.js.map
--- web/dist/assets/main-Ck5EiAT6.js
+++
@@ -1,2 +0,0 @@
-function o(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}console.log("hello world");export{o as __vite_legacy_guard};
-//# sourceMappingURL=main-Ck5EiAT6.js.map
